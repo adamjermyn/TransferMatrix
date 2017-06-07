55bfa9d48167f684aabbd4e5b368860775c421a7
Adam Jermyn <adamjermyn@gmail.com>
2017-06-07 17:48:50 +0100

    Updated print statements.

--- test.py
+++ test.py
@@ -7,9 +7,9 @@ stateRange = [0,1]
 params = tmc.sp.symbols('p q w')
 tm,left,right,blockSize = tmc.transferMatrixVariableSize(models.eFuncOnePlaneActinModel,stateRange,params)
 T,eL,eR = tmc.wrapper(tm,left,right,params)
-print tmc.fN(T,eL,eR,[0,4,0],blockSize,6)
+print(tmc.fN(T,eL,eR,[0,4,0],blockSize,6))
 
 s = 0
 for i in it.product([0,1],repeat=6):
 	s += np.exp(-models.eFuncOnePlaneActinModel(i,[0,4,0]))
-print -np.log(s)
+print(-np.log(s))
